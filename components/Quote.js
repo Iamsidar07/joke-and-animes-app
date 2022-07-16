c55aca36c96472f0009dbee634a5ac2b7adecd1c
Manoj <manojkumarprayasjee47@gmail.com>
2022-07-16 07:27:54 -0800

    improved jokes and quotes

--- components/Quote.js
+++ components/Quote.js
@@ -3,23 +3,23 @@ import { Fade } from "react-reveal";
 
 const Quote = ({ quotes }) => {
   return (
-    <div className="inline-grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-1 md:p-3 ">
+    <div className="inline-grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  md:p-3 ">
       {quotes?.length == 0 ? (
         <p>Loading</p>
       ) : (
         quotes?.map((quote) => {
           return (
             <Fade key={quote?.quote} bottom>
-              <div className="p-2 rounded md:hover:border border-sky-400 inline text-white bg-[#1e293b] group my-1 md:m-3  md:p-3  transition-all duration-200 ease-in transform cursor-pointer  ">
+              <div className="p-1 rounded inline  text-white bg-[#1e293b] group my-1 md:m-3  md:p-3 md:hover:bg-black/5 transition-transform duration-200 ease-in transform cursor-pointer  ">
                 <h1 className="font-bold text-3xl  my-2 ">
                   <small className=" text-xl  font-normal text-gray-400">
                     character-
                   </small>
                   {quote.character}
                 </h1>
-                <p>{quote?.quote}</p>
+                <p className='italic p-1 border-l-4 border-pink-500'>{quote?.quote}</p>
                 <p className=" font-semibold md:opacity-0 md:group-hover:opacity-100 duration-500 ">
-                  <small className="text-gray-400 ">Anime-</small>
+                  <small className="text-gray-400  ">Anime-</small>
                   {quote?.anime}
                 </p>
               </div>
