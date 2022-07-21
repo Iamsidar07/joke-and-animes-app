84771e8112d1f62d2ed8ddeec893b607bef5329e
Manoj <manojkumarprayasjee47@gmail.com>
2022-07-21 13:21:36 -0800

    look nicer

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,7 +29,7 @@ function scrollFunction() {
 }
 
   return (
-    <div className={navbg?"bg-gray-900 w-full md:bg-slate-100 md:h-20 text-white  shadow-sm z-[10]   p-2 sticky top-0 left-0 right-0 md:transition-all ease-in md:duration-500":"bg-gray-900 md:bg-transparent md:h-20 text-white  shadow-sm z-[10]  w-full p-2 sticky top-0 left-0 right-0 md:transition-all ease-in md:duration-500"}>
+    <div className={navbg?"bg-gray-900 w-full md:bg-slate-100 md:h-20 text-white  shadow-sm z-[10]   p-2 sticky top-0 left-0 right-0 md:transition-all ease-in md:duration-500":"bg-transparent md:h-20 text-white  shadow-sm z-[10]  w-full p-2 sticky top-0 left-0 right-0 md:transition-all ease-in md:duration-500"}>
       <div className="flex  items-center justify-between   md:px-10 ">
         <Link href={"/"}>
           <div className="text-4xl p-2  font-serif justify-center  my-auto group cursor-pointer flex items-center animate-pulse md:hover:animate-spin">
